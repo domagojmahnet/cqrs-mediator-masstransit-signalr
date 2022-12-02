e98c8d541d9c20e0c71eb99ff1fe70ae22cf4f87
Domagoj Mahnet <domagoj.mahnet@king-ict.cloud>
2022-12-02 12:40:08 +0100

    Copied project

---
+++ EKupi.Publisher/Controllers/OrderController.cs
@@ -0,0 +1,48 @@
+﻿using EKupi.Application.Hubs;
+using EKupi.Publisher.DTOs;
+using EKupi.Shared;
+using MassTransit;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace EKupi.Publisher.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IPublishEndpoint _publishEndpoint;
+        private readonly IHubContext<GuidGeneratorHub> _hub;
+
+        public OrderController(IPublishEndpoint publishEndpoint, IHubContext<GuidGeneratorHub> hub)
+        {
+            _publishEndpoint = publishEndpoint;
+            _hub = hub;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(OrderDto orderDto)
+        {
+            await _publishEndpoint.Publish<IOrderCreated>(new
+            {
+                orderDto.CustomerId,
+                orderDto.OrderDetails
+            });
+            return Ok();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> TestHub()
+        {
+            foreach (var item in Enumerable.Range(1, 100))
+            {
+                await _hub.Clients.All.SendAsync("NewGuid", Guid.NewGuid());
+                await Task.Delay(100);
+            }
+            return Ok();
+        }
+    }
+}
---
+++ EKupi.WebApi/Controllers/OrdersController.cs
@@ -0,0 +1,81 @@
+﻿using EKupi.Application.Hubs;
+using EKupi.Application.Orders.Commands;
+using EKupi.Application.Orders.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System.Net;
+
+namespace EKupi.WebApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IHubContext<GuidGeneratorHub> _hub;
+
+        public OrdersController(IMediator mediator, IHubContext<GuidGeneratorHub> hub)
+        {
+            _mediator = mediator;
+            _hub = hub;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrders()
+        {
+            return Ok(await _mediator.Send(new OrderQuery()));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(CreateOrderCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(long id)
+        {
+            return Ok(await _mediator.Send(new DeleteOrderCommand(id)));
+        }
+
+        [HttpGet("mostSoldProducts")]
+        public async Task<IActionResult> GetMostSoldProducts()
+        {
+            return Ok(await _mediator.Send(new MostSoldProductsQuery()));
+        }
+
+        [HttpGet("customerExpenditure")]
+        public async Task<IActionResult> GetCustomerExpenditure()
+        {
+            return Ok(await _mediator.Send(new CustomersExpenditureQuery()));
+        }
+
+        [HttpGet("productSalesPerMonth")]
+        public async Task<IActionResult> GetProductSalesPerMonth()
+        {
+            return Ok(await _mediator.Send(new ProductSalesPerMonthQuery()));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> EditOrder(EditOrderCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+
+        [HttpGet("testsignalr")]
+        [AllowAnonymous]
+        public async Task<IActionResult> TestHub()
+        {
+            foreach (var item in Enumerable.Range(1, 1000))
+            {
+                await _hub.Clients.All.SendAsync("NewGuid", Guid.NewGuid());
+                await Task.Delay(100);
+            }
+            return Ok();
+        }
+    }
+}
